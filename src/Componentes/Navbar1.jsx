8330e5a0e936edebcb40ae825fe9197d659d9600
roramoss <ramosesperanzarocio@gmail.com>
2024-05-04 17:14:27 -0300

    agrego navbar con logo

---
+++ src/Componentes/Navbar1.jsx
@@ -0,0 +1,28 @@
+
+
+function Nav() {
+  return (
+    <>
+    
+    <div className="navbar">
+    <img
+              src="src/assets/logo_transparent.png"
+              width="90"
+              height="90"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Acerca de</a></li>
+        <li><a href="#">Servicios</a></li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+    </div>
+        
+     
+    </>
+  );
+}
+
+export default Nav;
